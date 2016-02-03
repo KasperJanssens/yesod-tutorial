62ec0b317bd191cb0df2152129770afb22c8d4eb
Kasper Janssens <kasper.janssens.android@gmail.com>
2016-02-03 12:17:38 +0000

    Beetje meer css stuff, nog altijd niet goed

--- Handler/Worksheet.hs
+++ Handler/Worksheet.hs
@@ -6,48 +6,29 @@ import Data.Time.LocalTime
 amplidata :: Company
 amplidata = Company "amplidata" "niks"
 
-
 selectFields:: [(Company, Company)]
 selectFields = [(amplidata,amplidata)]
--- selectFields :: [(Text, Text)]
--- selectFields = []
--- selectFields = [("Value 1" :: Text, "value1"),("Value 2", "value2")]
 
 getWorksheetR :: Handler Html
 getWorksheetR = do
-  ((_result, formWidget), formEnctype) <- runFormGet dateForm2
+  ((_result, dateFormWidget), dateFormEnctype) <- runFormGet dateForm
+  ((_result, companyFormWidget), companyFormEnctype) <- runFormGet companyForm
   defaultLayout $ do
     setTitle "Welcome To Worksheets!"
     $(widgetFile "worksheet")
 
 postWorksheetR :: Handler Html
 postWorksheetR = do
-  ((_result, formWidget), formEnctype) <- runFormPost dateForm2
+  ((_result, dateFormWidget), dateFormEnctype) <- runFormPost dateForm
+  ((_result, companyFormWidget), companyFormEnctype) <- runFormGet companyForm
   defaultLayout $ do
      setTitle "Welcome To Worksheets!"
      $(widgetFile "worksheet")
 
 
+companyForm :: Form Company
+companyForm = renderBootstrap3 BootstrapBasicForm $ areq (selectFieldList selectFields) "" Nothing
 
--- dateForm2 :: Html -> MForm Handler (FormResult ((TimeOfDay,TimeOfDay), Company), Widget)
-dateForm2 :: Html -> MForm Handler ((FormResult Company), Widget)
-dateForm2 extra = do
-  (comboRes, comboView) <- mreq (selectFieldList selectFields) "combo" Nothing
---   (fromRes, fromView) <- mreq timeFieldTypeTime "Choose start time" Nothing
---   (toRes, toView) <- mreq timeFieldTypeTime "Choose end time" Nothing
---   let fromToRes = (,) <$> fromRes <*> toRes
---   let fromToComboRes = (,) <$> fromToRes <*> comboRes
-  let widget = do
-          toWidget [whamlet|
-              #{extra}
-              <p> Today's work for
-                  \ #
-                  ^{fvInput comboView}
-          |]
-  return (comboRes, widget)
-
-dateForm :: Form (TimeOfDay, TimeOfDay)
-dateForm = renderBootstrap3 BootstrapBasicForm $ (,)
-               <$> areq timeFieldTypeTime "Choose start time" Nothing
-               <*> areq timeFieldTypeTime "Choose end time" Nothing
+dateForm :: Form  TimeOfDay
+dateForm = renderBootstrap3 BootstrapBasicForm $  areq timeFieldTypeTime "" Nothing
 
