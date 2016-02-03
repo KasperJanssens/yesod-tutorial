31e2af62296585d3ee8a28c58de7dd3ae09967ca
Kasper Janssens <kasper.janssens.android@gmail.com>
2016-02-03 18:21:42 +0000

    InputForm met csrf bescherming

--- Handler/Worksheet.hs
+++ Handler/Worksheet.hs
@@ -1,7 +1,6 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Handler.Worksheet where
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Data.Time.LocalTime
 
 amplidata :: Company
 amplidata = Company "amplidata" "niks"
@@ -11,26 +10,22 @@ selectFields = [(amplidata,amplidata)]
 
 getWorksheetR :: Handler Html
 getWorksheetR = do
-  (dateFormWidget, dateFormEnctype) <- generateFormPost dateForm
-  (companyFormWidget, companyFormEnctype) <- generateFormPost companyForm
+  req <- getRequest
+  let token =
+          case reqToken req of
+              Nothing -> "lebberlebber"
+              Just n -> n
   defaultLayout $ do
     setTitle "Welcome To Worksheets GETTED!"
     $(widgetFile "worksheet")
 
-postWorksheetR :: Handler Html
-postWorksheetR = do
-  ((dateResult, dateFormWidget), dateFormEnctype) <- runFormPostNoToken dateForm
-  ((companyResult, companyFormWidget), companyFormEnctype) <- runFormPostNoToken companyForm
-  print dateResult
-  print companyResult
-  defaultLayout $ do
-     setTitle "Welcome To Worksheets POSTED!"
-     $(widgetFile "worksheet")
 
+postInputR :: Handler Html
+postInputR = do
+    dateTime <- runInputPost $ ireq timeFieldTypeTime "dateTimePicker"
+    company <- runInputPost $ ireq (selectFieldList selectFields) "companyPicker"
+    defaultLayout [whamlet|<p>#{show dateTime}
+                           <p>#{show company}
+    |]
 
-companyForm :: Form Company
-companyForm = renderBootstrap3 BootstrapBasicForm $ areq (selectFieldList selectFields) "" Nothing
-
-dateForm :: Form  TimeOfDay
-dateForm = renderBootstrap3 BootstrapBasicForm $  areq timeFieldTypeTime "" Nothing
 
