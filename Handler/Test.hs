f79612355c58b5d64fc962ed07c716dd6d328551
kasper <kasper.janssens.android@gmail.com>
2016-04-01 17:29:46 +0000

    test.julius toegevoegd

--- Handler/Test.hs
+++ Handler/Test.hs
@@ -15,32 +15,53 @@ textFieldList = Field
   {
     fieldParse = \texts _ ->
       do
-        print "veldje geparsed"
         return $ Right . Just $ texts ,
     fieldView = \theId name attrs val isReq ->
       let zipUp = zip ([1..]::[Int]) in
       [whamlet|
       <p> Dit is de lijst
-      <ul>
+      <button id="#{voegToe}"> voeg toe
+      <ul id="#{ulId}">
       $case val
         $of Left errMsg
           <li>
-            <p> twas ne left
-            <input id=#{theId}-1 *{attrs} type=text name=#{name} value=#{errMsg} checked>
+            <input id=#{theId}-7 *{attrs} type=text name=#{name} value=#{errMsg} checked>
+          <li>
+            <input id=#{theId}-8 *{attrs} type=text name=#{name} value=#{errMsg} checked>
+          <li>
+            <input id=#{theId}-9 *{attrs} type=text name=#{name} value=#{errMsg} checked>
         $of Right textVals
           $forall (idPostFix, textVal) <- zipUp textVals
             <li>
-              <p> twas ne right
               <input id=#{theId}-#{idPostFix} *{attrs} type=text name=#{name} value=#{textVal} checked>
       |]
     ,
     fieldEnctype = UrlEncoded
   }
 
+testFormId = ("testFormId"::Text)
+
+voegToe = ("voegEensEenTextekeToe"::Text)
+
+ulId = ("ulleke"::Text)
+
+
 getTestR :: Handler Html
 getTestR = do
     (testFormWidget, formEnctype) <- generateFormPost testForm
-    let testFormId = ("testFormId"::Text)
     defaultLayout $ do
         setTitle "Welcome To Yesod!"
         $(widgetFile "test")
+
+-- The POST handler processes the form. If it is successful, it displays the
+-- parsed person. Otherwise, it displays the form again with error messages.
+postTestR :: Handler Html
+postTestR = do
+    ((result, testFormWidget), enctype) <- runFormPost testForm
+    case result of
+        FormSuccess texties -> defaultLayout [whamlet| $forall text <- texties
+                                                             <p> #{text}
+                                                |]
+        _ -> defaultLayout $ do
+                 setTitle "Let's try again"
+                 $(widgetFile "test")
